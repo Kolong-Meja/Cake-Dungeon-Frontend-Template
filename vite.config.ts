3bd7ee468b41fb2385b86c624de3fe4c5c331f98
Faisal Ramadhan <faisalramadhan1299@gmail.com>
2024-07-14 17:38:40 +0700

    preview test

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,28 @@ export default defineConfig({
 	server: {
 		host: '127.0.0.1',
 		port: 3000,
-		strictPort: true
+		strictPort: true,
+		cors: {
+			origin: true,
+			methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+			credentials: true,
+			allowedHeaders: ['Content-Type', 'X-Auth-Token', 'Origin', 'Authorization'],
+			maxAge: 86400
+		}
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	preview: {
+		host: '127.0.0.1',
+		port: 3002,
+		strictPort: true,
+		cors: {
+			origin: true,
+			methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+			credentials: true,
+			allowedHeaders: ['Content-Type', 'X-Auth-Token', 'Origin', 'Authorization'],
+			maxAge: 86400
+		}
 	}
 });
